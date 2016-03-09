da2233cc5b5d86b87495a7f5f277fc1f4d4c025f
Chris Steinbacher <steinbacherdesign@gmail.com>
2016-03-09 14:08:40 -0800

    installed a yeoman generator for angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mfhUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mfhUiApp
+ */
+angular.module('mfhUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mfhUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mfhUiApp
+ */
+angular.module('mfhUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
