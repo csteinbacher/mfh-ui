b99179a54b2b9d562c6471b9adace8b632ce6d2c
Chris Steinbacher <steinbacherdesign@gmail.com>
2016-03-15 13:56:08 -0700

    followed some tutorial stuff to get startedd 2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,7 +18,55 @@ angular.module('mfhUiApp')
 
 		$scope.removeTodo = function (index){
 			$scope.todos.splice(index, 1);
-		}
+		};
+
+	}).controller('SidebarController',function($scope){
+		$scope.state=false;
+
+		$scope.toggleState = function() {
+	        $scope.state = !$scope.state;
+	        console.log('foo');
+	    };
+});
+
+angular.module('mfhUiApp').directive('sidebarDirective',function(){
+	return {
+        link : function(scope, element, attr) {
+            scope.$watch(attr.sidebarDirective, function(newVal) {
+                  if(newVal)
+                  {
+                    element.addClass('show'); 
+                    return;
+                  }
+                  element.removeClass('show');
+            });
+        }
+    };
+
+});
 
 
-  });
+//var app = angular.module('mfhUiApp', []);
+
+// mfhUiApp.controller('SidebarController', function($scope){
+// 	$scope.state = false;
+    
+//     $scope.toggleState = function() {
+//         $scope.state = !$scope.state;
+//     };
+// });
+
+// mfhUiApp.directive('sidebarDirective', function() {
+//     return {
+//         link : function(scope, element, attr) {
+//             scope.$watch(attr.sidebarDirective, function(newVal) {
+//                   if(newVal)
+//                   {
+//                     element.addClass('show'); 
+//                     return;
+//                   }
+//                   element.removeClass('show');
+//             });
+//         }
+//     };
+// });
